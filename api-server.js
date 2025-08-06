d273db3b04ead0d431e21dcc68c57572db7ef473
Ask Any Expert Team <user@askanyexpert.local>
2025-08-06 19:00:47 +0800

    Change default port to 8080 for Railway

--- api-server.js
+++ api-server.js
@@ -4,7 +4,7 @@ const workflowEngine = require("./src/workflow-engine");
 const logger = require("./src/utils/logger");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
